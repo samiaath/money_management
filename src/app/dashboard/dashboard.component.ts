b001f074dd7c30c661285eedd33b8eac8b9815db
mohamed ali <mohamedali.chibani@enicar.ucar.tn>
2024-12-28 11:57:06 +0100

    donut chart is done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
